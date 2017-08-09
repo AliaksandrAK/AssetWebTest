9d810134b7e4fce4af39fa1eac7241156d68833e
Aliaksandr Kusonski <akusonski@servicechannel.com>
2017-08-09 22:46:17 +0300

    Convert Value to money

--- AssetWebTest/Controllers/HomeController.cs
+++ AssetWebTest/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace AssetWebTest.Controllers
 {
@@ -25,6 +26,27 @@ namespace AssetWebTest.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public class TestAsset
+        {
+            public int? AssetId { get; set; }
+
+            public string Name { get; set; }
+            public decimal? Money { get; set; }
+
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public JsonResult GetTest(int proposalId)
+        {
+            TestAsset aTest = new TestAsset();
+            aTest.AssetId = default(int?);
+            aTest.Name = string.Empty;
+            aTest.Money = 123456.98m;
+            var jsonSerialiser = new JavaScriptSerializer();
+            var jsonPR = jsonSerialiser.Serialize(aTest);
+
+            return new JsonResult { Data = new { jsonPR, isSuccess = true }, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
     }
 }
