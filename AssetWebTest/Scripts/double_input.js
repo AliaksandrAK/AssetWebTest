aa84b7016bb0ba51018c4f0cadd221e0447c87f8
Aliaksandr Kusonski <akusonski@servicechannel.com>
2017-12-09 14:42:26 +0300

    Update double JS

--- AssetWebTest/Scripts/double_input.js
+++ AssetWebTest/Scripts/double_input.js
@@ -4,11 +4,14 @@
  * @version: 1.0 - 2017-12-07
  *
  * Created by Alex Kusonski
+ * Comments: When paste data from clipboard there is not checking to put only number (it is not correct).
+ * It is because old version for the input item work the same and client should see the same in new version.
+ * Call FormatDecimal (Proposal.Web/Scripts/Custom/prototype/basics.js) after paste or set callbackfunc. It change the text to 0.0.
  *
  * Copyright (c) ServiceChannel
  *
  */
-function double_input(selector, cel, dec) {
+function double_input(selector, cel, dec, callbackfunc) {
     var sep = ".";
     var symbolTh = ",";
     var symbolMinus = "-";
@@ -50,6 +53,7 @@ function double_input(selector, cel, dec) {
         }
     });
     $(selector).bind('paste', function (e) {
+        var result = true;
         var start = e.target.selectionStart;
         var end = e.target.selectionEnd;
         var val = e.target.value;
@@ -67,7 +71,7 @@ function double_input(selector, cel, dec) {
             if (arr.length > 0) {
                 if (arr[0].length > cel) {
                     arr[0] = arr[0].substring(0, cel);
-                    cutstring = arr[0] + sep;
+                    cutstring = arr[0];
                 }
                 else if (arr.length === 1) {
                     cutstring = arr[0] + sep;
@@ -77,17 +81,23 @@ function double_input(selector, cel, dec) {
                     cutstring = arr[0] + sep + arr[1];
                 } 
             }
-            return true;
+            $(this).val(cutstring);
+            if (callbackfunc != null) callbackfunc($(this));
+            result = false;
         }
-        var mergeval = insert(val, start, pastedData);
-        var mergarr = mergeval.split(sep);
-        if (mergarr.length === 1 && mergarr[0].length > cel) return false;
-        if (mergarr.length === 2) {
-            mergarr[0] = mergarr[0].replace(symbolTh, "");
-            if (mergarr[0].length > cel) return false;
-            if (mergarr[1].length > dec) return false;
+        else {
+            var mergeval = insert(val, start, pastedData);
+            var mergarr = mergeval.split(sep);
+            if (mergarr.length === 1 && mergarr[0].length > cel) result = false;
+            else if (mergarr.length === 2) {
+                    mergarr[0] = mergarr[0].replace(symbolTh, "");
+                    if (mergarr[0].length > cel) result = false;
+                    if (mergarr[1].length > dec) result = false;
+                }
+            else if (mergarr.length > 2) result = false;
         }
-        if (mergarr.length > 2) return false;
+
+        return result;
     });
 }
 
