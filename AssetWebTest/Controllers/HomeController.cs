93768b76a322d061c5faf1d72741934e64964311
Aliaksandr <relon@tut.by>
2018-01-22 17:36:27 +0300

    Add Camera

--- AssetWebTest/Controllers/HomeController.cs
+++ AssetWebTest/Controllers/HomeController.cs
@@ -119,5 +119,31 @@ namespace AssetWebTest.Controllers
                 return Json(new { status = "error", message = ex.Message });
             }
         }
+
+        public void Capture()
+        {
+            var stream = Request.InputStream;
+            string dump;
+
+            using (var reader = new StreamReader(stream))
+                dump = reader.ReadToEnd();
+
+            var path = Server.MapPath("~/test.jpg");
+            System.IO.File.WriteAllBytes(path, String_To_Bytes2(dump));
+        }
+
+        private byte[] String_To_Bytes2(string strInput)
+        {
+            int numBytes = (strInput.Length) / 2;
+            byte[] bytes = new byte[numBytes];
+
+            for (int x = 0; x < numBytes; ++x)
+            {
+                bytes[x] = Convert.ToByte(strInput.Substring(x * 2, 2), 16);
+            }
+
+            return bytes;
+        }
+
     }
 }
